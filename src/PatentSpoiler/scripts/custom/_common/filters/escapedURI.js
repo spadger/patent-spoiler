0fd6cc59e7a2f07744f13f9746a3678fee89328f
Jon Bates <spadger@gmail.com>
2014-06-28 13:36:55 +0100

    Fixed a util error

--- src/PatentSpoiler/scripts/custom/_common/filters/escapedURI.js
+++ src/PatentSpoiler/scripts/custom/_common/filters/escapedURI.js
@@ -1,6 +1,6 @@
 ﻿///<reference path="~/scripts/custom/_common/UtilsModule.js"/>
 'use strict';
 window.utilsModule.filter('escapedURI', ['$window', function($window) {
-    return window.encodeURIComponent;
+    return $window.encodeURIComponent;
 }]);
  
