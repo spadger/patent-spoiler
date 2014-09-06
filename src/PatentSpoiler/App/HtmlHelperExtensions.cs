bee97a12763e370587697ae85e5bc999fc3c009c
Jon Bates <spadger@gmail.com>
2014-09-06 23:16:31 +0100

    Added a HTML.ToJson() method

--- src/PatentSpoiler/App/HtmlHelperExtensions.cs
+++ src/PatentSpoiler/App/HtmlHelperExtensions.cs
@@ -1,10 +1,12 @@
 ﻿using System.Web;
 using System.Web.Mvc;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
 using PatentSpoiler.App.Domain.Security;
 
 namespace PatentSpoiler.App
 {
-    public static class HtmlHelperExtnesions
+    public static class HtmlHelperExtensions
     {
         public static bool UserIsAuthenticated(this HtmlHelper @this)
         {
@@ -14,6 +16,11 @@ namespace PatentSpoiler.App
         public static bool UserIs(this HtmlHelper @this, UserRole role)
         {
             return @this.UserIsAuthenticated() && HttpContext.Current.User.IsInRole(role.ToString());
+        }
+
+        public static string ToJson(this HtmlHelper @this, object model)
+        {
+            return JsonConvert.SerializeObject(model, new JsonSerializerSettings { ContractResolver = new CamelCasePropertyNamesContractResolver() });
         }
     }
 }
