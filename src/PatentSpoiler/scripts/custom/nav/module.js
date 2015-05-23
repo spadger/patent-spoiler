9b8fedfe0b5e9d576a2ca9ae9b41e7d58e6798a7
Jon Bates <spadger@gmail.com>
2015-05-24 00:00:51 +0100

    Fixed mobile layout and added a nav-ng-app

---
+++ src/PatentSpoiler/scripts/custom/nav/module.js
@@ -0,0 +1,10 @@
+﻿///<reference path="~/scripts/angular.js" />
+/// <reference path="~/scripts/ui-bootstrap.js" />
+'use strict';
+angular.module('nav', ['ui.bootstrap']);
+
+angular.module('nav').controller('nav', ['$scope', function($scope) {
+
+    $scope.abc = 'def';
+
+}]);
---
+++ src/PatentSpoiler/scripts/custom/nav/navController.js
@@ -0,0 +1,8 @@
+﻿///<reference path="module.js" />
+
+'use strict';
+angular.module('nav').controller('nav', ['$scope', function($scope) {
+
+    $scope.abc = 'def';
+
+}]);
